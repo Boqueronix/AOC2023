29eceb766cbc34a8a7c907d3648cb3cf0bc436c6
juangs27 <juangs27@vt.edu>
2023-12-12 17:35:01 -0500

    Day 6

--- src/Day6/Day6Challenge2.java
+++ src/Day6/Day6Challenge2.java
@@ -9,9 +9,9 @@ public class Day6Challenge2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter: ");
         String input = scanner.nextLine();
-        Long time = Long.parseLong(input.substring(5).replace(" ", ""));
+        long time = Long.parseLong(input.substring(5).replace(" ", ""));
         input = scanner.nextLine();
-        Long record = Long.parseLong(input.substring(9).replace(" ", ""));
+        long record = Long.parseLong(input.substring(9).replace(" ", ""));
         System.out.println("Product: " + calculatePosibilities(new Race(time, record)));
     }
 
